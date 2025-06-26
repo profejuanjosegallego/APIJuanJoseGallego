eb0af9ba31ff2febda77a9c8179a8bc99675c385
profetania <bamira1@misena.edu.co>
2025-06-26 14:30:23 -0500

    se prueba el api

--- SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/controladores/UsuarioControlador.java
+++ SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/controladores/UsuarioControlador.java
@@ -5,6 +5,7 @@ import com.example.SpringBootJuanfe13.servcios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class UsuarioControlador {
     //FUNCION para activar el servicio de
     //guardado
 
+    @PostMapping
     public ResponseEntity<?> activarGuardado(@RequestBody Usuario datosEnviados){
         try{
             return ResponseEntity
--- SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/modelos/Usuario.java
+++ SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/modelos/Usuario.java
@@ -34,4 +34,52 @@ public class Usuario {
         this.edad = edad;
         this.fechaNacimiento = fechaNacimiento;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
